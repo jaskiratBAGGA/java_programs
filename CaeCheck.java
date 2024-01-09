3ac034985f6aa9131eb860274127f9a91b85d616
jaskirat_bagga <111983923+jaskiratBAGGA@users.noreply.github.com>
2024-01-09 23:27:15 +0530

    Add files via upload

---
+++ CaeCheck.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class CaeCheck {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string : ");
+
+        char ch = sc.next().trim().charAt(0);
+
+        if (ch >= 'a' && ch <= 'z') {
+            System.out.println("lowercase");
+        }
+
+        else {
+            System.out.println("uppercase");
+        }
+    }
+}
