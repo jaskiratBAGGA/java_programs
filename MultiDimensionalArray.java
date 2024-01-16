5791437fa60103f06f66f47f7a084bb9454f98a0
jaskirat_bagga <111983923+jaskiratBAGGA@users.noreply.github.com>
2024-01-16 23:21:39 +0530

    Add files via upload

---
+++ MultiDimensionalArray.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MultiDimensionalArray {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);  
+        int [][] arr = {
+                {2,3,4},
+                {5,6},
+                {7,8,9}
+        };
+        for(int row = 0; row<arr.length; row++){
+            for(int col=0; col<arr[row].length;col++){
+                arr[row][col] = in.nextInt();
+
+            }
+        }
+    }
+
+}
