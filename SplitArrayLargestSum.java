4197634728964beaaa7dc6d132aa10299b91d803
jaskirat_bagga <111983923+jaskiratBAGGA@users.noreply.github.com>
2024-02-07 00:02:05 +0530

    Add files via upload

---
+++ SplitArrayLargestSum.java
@@ -0,0 +1,45 @@
+public class SplitArrayLargestSum {
+    public static void main(String[] args) {
+        int [] nums = {7,2,5,10,8};
+        int m = 2;
+        System.out.println(splitArray(nums, m));
+
+    }
+    public static int splitArray(int[] nums, int m) {
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < nums.length; i++) {
+            start = Math.max(start, nums[i]);// in the end of the loop this will contain the max item from the array
+            end  += nums[i];
+
+        }
+        //binary search
+        while(start < end){
+            //try for middle as potential answer
+            int mid = start + (end - start) /2;
+
+            //calculate how many pieces u can divide this in with this max sum
+
+            int sum = 0;
+            int pieces = 1;
+
+            for(int num : nums){
+                if(sum + num > mid){
+                    //you cannot add this in this subarray
+                    //say u add this num in new subarray, then sum = num
+                    sum = num;
+                    pieces++;
+                }else{
+                    sum += num;
+                }
+            }
+            if(pieces > m){
+                start = mid + 1;
+
+            }else{
+                end = mid;
+            }
+        }
+        return end;
+    }
+}
